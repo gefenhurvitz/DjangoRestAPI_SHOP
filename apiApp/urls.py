1c042746d731821248742897711ab9c3d9c6e109
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-04-10 14:57:15 +0300

    adding serializer and views functions

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import hello_world
+from rest_framework.urlpatterns import format_suffix_patterns
+from apiApp.views import product_list
+
 
 urlpatterns = [
-    path('hello/', hello_world),
+    path('products/', product_list, name='product_list'),
+    # path('products/<int:pk>/', ProductDetail.as_view(), name='product_detail'),
+    # path('orders/', OrderList.as_view(), name='order_list'),
+    # path('orders/<int:pk>/', OrderDetail.as_view(), name='order_detail'),
+    # path('users/', UserList.as_view(), name='user_list'),
+    # path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- apiApp/views.py
+++ apiApp/views.py
@@ -1,6 +1,75 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product, Order, OrderItem
+from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+
+from django.views.decorators.csrf import csrf_protect
+
+
+
+@csrf_protect
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'Hello, World!'})
+def product_detail(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def product_create(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def product_update(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def product_delete(request, pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    return Response('Product deleted successfully')
+
+@api_view(['POST'])
+def order_create(request):
+    serializer = OrderSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_detail(request, pk):
+    order = Order.objects.get(id=pk)
+    serializer = OrderSerializer(order, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def order_update(request, pk):
+    order = Order.objects.get(id=pk)
+    serializer = OrderSerializer(instance=order, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def order_delete(request, pk):
+    order = Order.objects.get(id=pk)
+    order.delete()
+    return Response('Order deleted successfully')
